f2e7c0545a87005d8fbb14a52c3c5c97aedd24d3
kiriquellK <kiryaep@gmail.com>
2023-11-06 23:16:09 +0300

    fix 6.3

---
+++ DZ6.3/DZ6.3/Car.cs
@@ -0,0 +1,49 @@
+﻿using System.Formats.Asn1;
+
+namespace Classes
+{
+    internal class Car
+    {
+        internal int _year;
+        internal string _type;
+        internal string _colour;
+        internal Car() { }
+
+        internal Car(int establishYear, string type, string colour)
+        {
+            _year = establishYear;
+            _type = type;
+            _colour = colour;
+        }
+        internal void EngineOn()
+        {
+            Console.WriteLine("Автомобиль заведен");
+        }
+        internal void EngineOff()
+        {
+            Console.WriteLine("Автомобиль заглушен");
+        }
+        internal void SetYear(int newYear)
+        {
+            _year = newYear;
+            Console.WriteLine($"Установлен год выпуска: {_year}");
+        }
+        internal void SetType(string newType)
+        {
+            _type = newType;
+            Console.WriteLine($"Установлен тип кузова: {_type}");
+        }
+        internal void SetColour(string newColour)
+        {
+            _colour = newColour;
+            Console.WriteLine($"Установлен цвет автомобиля: {_colour}");
+        }
+        internal void GetCarInfo(int year = 1991, string type = "Universal", string colour = "violet")
+        {
+            _year = year;
+            _type = type;
+            _colour = colour;
+            Console.WriteLine($"Год выпуска - {_year}{Environment.NewLine}Тип кузова - {_type}{Environment.NewLine}Цвет кузова - {_colour}");
+        }
+    }
+}
---
+++ DZ6.3/DZ6.3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Formats.Asn1;
+
+namespace Classes
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isContinue = true;
+            while (isContinue)
+            {
+                Console.WriteLine($"Выберите действие над автомобилем:{Environment.NewLine}0 - Завести автомобиль{Environment.NewLine}1 - Заглушить автомобиль{Environment.NewLine}2 - Установить год выпуска автомобиля{Environment.NewLine}3 - Установить тип кузова автомобиля{Environment.NewLine}4 - Установить цвет автомобиля");
+                bool isInputNumber = int.TryParse(Console.ReadLine(), out int actionNumber);
+                if (!isInputNumber)
+                {
+                    Console.WriteLine("Некорректый выбор");
+                }
+                else
+                {
+                    switch (actionNumber)
+                    {
+                        case 0:
+                            Car start = new Car();
+                            start.EngineOn();
+                            start.GetCarInfo();
+                            break;
+                        case 1:
+                            Car off = new Car();
+                            off.EngineOn();
+                            off.GetCarInfo();
+                            break;
+                        case 2:
+                            Console.WriteLine("Введите год выпуска");
+                            bool isInputYear = int.TryParse(Console.ReadLine(), out int actualYear);
+                            if (isInputYear)
+                            {
+                                Car setYear = new Car();
+                                setYear.SetYear(actualYear);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Введен некорректный год выпуска");
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine("Введите тип кузова");
+                            string inputType = Console.ReadLine();
+                            if (!string.IsNullOrWhiteSpace(inputType))
+                            {
+                                string type = inputType;
+                                Car setType = new Car();
+                                setType.SetType(type);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Введен некорректный тип кузова");
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("Введите цвет кузова");
+                            string inputColour = Console.ReadLine();
+                            if (!string.IsNullOrWhiteSpace(inputColour))
+                            {
+                                string colour = inputColour;
+                                Car setColour = new Car();
+                                setColour.SetColour(colour);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Введен некорректный цвет кузова");
+                            }
+                            break;
+                        default:
+                            Car defaultCar = new Car(1991, "Universal", "Violet");
+                            defaultCar.GetCarInfo();
+                            isContinue = false;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
